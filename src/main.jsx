7d0c9c94bda90532faf0858df7b273551a9ed348
wanliu2021 <1692894189@qq.com>
2022-08-01 11:29:12 +0800

    Adding a "No Match Route"

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,23 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 import "./main.css";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
 
 let rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render((
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>}/>
+      <Route path="/expenses" element={<Expenses/>}/>
+      <Route path="/invoices" element={<Invoices/>}/>
+      <Route path="*" element={
+      <main style={{padding:"1rem"}}>
+        <p>There's nothing here!</p>
+      </main> 
+    }/>
+    </Routes>
+    
+  </BrowserRouter>
+), rootElement);
